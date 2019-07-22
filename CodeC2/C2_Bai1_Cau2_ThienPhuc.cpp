eb2cfbcd1e537b9bcf80f63c672506ecdebf59e1
phuc <1851050115phuc@ou.edu.vn>
2019-07-22 07:14:29 +0700

    Nguyễn Thiên Phúc

---
+++ CodeC2/C2_Bai1_Cau2_ThienPhuc.cpp
@@ -0,0 +1,177 @@
+//Project: C2_Bai1_Cau2.cpp
+#include <iostream>
+using namespace std;
+#define MAX 100
+int a[MAX];
+int n;
+
+void nhap(int a[], int &n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+void xuat(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << endl;
+	}
+}
+
+int tim(int a[], int n, int x)
+{
+	int i = 0;
+	while (i < n && a[i] != x)
+		i++;
+	if (i == n)
+		return -1;
+	else
+		return i;
+}
+
+int them1(int a[], int &n, int x)
+{
+	int i;
+	if ( n < MAX)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (a[i] > x)
+			{
+					for(int j = n - 1; j >= i; j--)
+						a[j + 1] = a[j];
+			}
+			
+		}
+		a[i] = x;
+		n++;
+		return 1;
+	}
+	return 0;
+}
+
+int them(int a[], int &n, int x, int i)
+{
+	if ( i<= n && n < MAX)
+	{
+		for(int j = n - 1; j >= i; j--)
+			a[j + 1] = a[j];
+		n++;
+		a[i] = x;
+		return 1;
+	}
+	return 0;
+}
+
+int xoa(int a[], int &n, int i)
+{
+	if ( i >= 0 && i < n)
+	{
+		for (int j = i; j < n - 1; j++)
+			a[j ] =a[j + 1];
+		n--;
+		return 1;
+	}
+	return 0;
+}
+
+bool timnhiphan(int a[], int n, int x)
+{
+	int mid , left = 0, right = n;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (a[mid] == x)
+			return true;
+		else
+			if(a[mid] < x)
+				right = mid - 1;
+			else
+				left = mid + 1;
+	}
+	return false;
+}
+
+int main()
+{
+	int x, i, t, chon;
+	bool ti;
+	do
+	{
+	cout << "Nhap so phan tu cua mang duoi 100 phan tu: ";
+	cin >> n;
+	if(n < 0 || n > 100)
+		cout << "Nhap sai. Nhap lai\n";
+	} while(n < 0 || n > 100);
+	while (true)
+	{
+		cout << "\nNhap chon:\n"
+			 << "0. Thoat\n"
+			 << "1. Nhap vao n phan tu\n"
+			 << "2. Them phan tu \n"
+			 << "3. Tim nhi phan\n"
+			 << "4. Tim\n"
+			 << "5. Xuat danh sach\n"
+			 << "6. Xoa phan tu\n"
+			 << "Chon: ";
+		cin >> chon;
+		switch(chon)
+		{
+		case 0:
+			return 1;
+			break;
+		case 1:
+			nhap(a, n);
+			break;
+		case 2:
+			cout << "Nhap gia tri can them vao mang: ";
+			cin >> x;
+			t = them1(a, n, x);
+			if (t != 0)
+				cout << "Them thanh cong\n";
+			else
+				cout << "Them khong thanh cong\n";
+			break;
+		case 3:
+			cout << "Nhap gia tri can tim vao mang: ";
+			cin >> x;
+			ti = timnhiphan(a, n, x);
+			if (ti == true)
+			{
+				cout<<"tim thanh cong\n";
+			}
+			else
+				cout<<"tim khong thanh cong\n";
+			break;
+		case 4:
+			cout << "Nhap vi tri can tim: ";
+			cin >> x;
+			t = tim(a, n, x);
+			if (t != -1)
+				cout << "Tim thay " << x << " o vi tri " << t << endl;
+			else
+				cout<<"Khong tim thay ";
+			break;
+		case 5:
+			xuat(a, n);
+			break;
+		case 6:
+			cout << "Nhap vi tri can xoa: ";
+			cin >> i;
+			t = xoa(a, n, i);
+			if(t != 0)
+				cout << "Xoa thanh cong\n";
+			else
+				cout << "Xoa khong thanh cong\n";
+			break;
+		default:
+			cout << "Gia tri chon khong hop le\n";
+			break;
+		}
+	}
+	return 0;
+}
---
+++ CodeC2/C2_Bai4_Cau13_ThienPhuc.cpp
@@ -0,0 +1,214 @@
+//Project: C2_Bai4_Cau13.cpp
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#define MAX 100
+
+int A[MAX], B[MAX], C[MAX];
+int n, m;
+
+int max(int m, int n) 
+{  
+	if (m > n)
+		return m;
+	else
+		return n;
+}
+
+void nhap(int A[], int &n)
+{
+	for (int i = 0; i < n + 1; i++)
+	{
+		cout << "Nhap he so thu " << i << " = ";
+		cin >> A[i];
+	}
+}
+
+void xuat(int a[], int n) 
+{ 
+    for (int i = 0; i < n + 1; i++) 
+    { 
+       cout << a[i]; 
+       if (i != 0) 
+		   cout << "x^" << i ; 
+       if (i != n) 
+		   cout << " + "; 
+    } 
+}
+
+int *add(int A[], int B[], int n, int m) 
+{ 
+   int size = max(n, m); 
+   int *sum = new int[size];
+   if (n > m)
+   {
+   for (int i = 0; i <= n; i++) 
+	   sum[i] = A[i]; 
+   for (int i = 0; i <= m; i++) 
+       sum[i] += B[i];  
+
+   }
+   else
+   {
+	   for (int i = 0; i < m + 1; i++) 
+		   sum[i] = B[i];
+	   for (int i = 0; i < n + 1; i++) 
+		   sum[i] += A[i];  
+   }
+     return sum; 
+}
+
+int *sub(int A[], int B[], int n, int m) 
+{ 
+	int size = max(m, n); 
+    int *hieu = new int[size]; 
+    for (int i = 0; i <= n; i++) 
+		hieu[i] = A[i]; 
+	for (int i = 0; i <= m; i++) 
+		hieu[i] -= B[i];  
+	return hieu; 
+}
+
+int *mul(int A[], int B[], int n, int m) 
+{ 
+	int *tich = new int[m + n - 1];  
+    for (int i = 0; i <= m + n - 1; i++) 
+	    tich[i] = 0; 
+    for (int i = 0; i <= n; i++) 
+    { 
+		for (int j = 0; j <= m; j++) 
+			tich[i + j] += A[i] * B[j]; 
+	} 
+	return tich; 
+}
+
+void chiadathuc(int A[],int B[],int n,int m)
+{
+	int i, j, z, x;
+    double r[MAX], du[MAX];
+	for (i = 0; i <= n; i++)
+		du[i] = A[i];
+       if (n < m)
+		   cout << 0 << "\n";
+       else
+	   {
+		   x = n;
+           z = n - m;
+           if (n == m)
+		   {
+			   r[z] = A[n] / B[n];
+			   z++;
+		   }
+           else
+		   {
+			   while (z >= 0)
+			   {
+				   r[z] = du[x] / B[m];
+                   int y = m;
+				   for (i = x; i >= x - m; i--)
+				   {
+					   du[i] = du[i] - r[z] * B[y];
+					   y--;
+				   }
+				   x--;
+				   z--;
+			   }
+		   }
+	   }
+       for (i = n - m; i >= 0; i--)
+	   {
+		   if (i == 0)
+			   cout << r[0] << "\n";
+		   else
+			   if(r[i] != 0)
+				   cout << r[i] << "*x^" << i << "+";
+	   }
+       cout << "Da thuc du: ";
+       for (i = x; i >= 0; i--)
+	   {
+		   if (i == 0)
+			   cout << du[0] << "\n";
+		   else
+			   if (du[i] != 0)
+				   cout << du[i] << "*x^" << i << "+";
+	   }
+}
+
+int main()
+{
+	int chon;
+	int *sum, *hieu, *tich;
+	int size;
+	while (true)
+	{
+		cout << "\nNhap chon:\n"
+ 			 << "0.Thoat\n"
+			 << "1. Nhap vao da thuc A\n"
+			 << "2. Nhap vao da thuc B\n"
+			 << "3. Xuat ca 2 da thuc\n"
+			 << "4. Tong 2 da thuc\n"
+			 << "5. Hieu 2 da thuc\n"
+			 << "6. Tich 2 da thuc\n"
+			 << "7. Thuong 2 da thuc\n"
+			 << "Chon: ";
+		cin >> chon;
+		switch(chon)
+		{
+		case 0:
+			return 1;
+			break;
+		case 1:
+			do
+			{
+				cout << "Nhap so mu cao nhat cua da thuc A: ";
+				cin >> n;
+				if (n < 0 || n > 100)
+					cout << "Nhap sai. Nhap lai\n";
+			} while(n < 0 || n > 100);
+			nhap(A, n);
+			break;
+		case 2:
+			do
+			{
+				cout << "Nhap so mu cao nhat cua da thuc B: ";
+				cin >> m;
+				if (m < 0 || m > 100)
+					cout << "Nhap sai. Nhap lai\n";
+			} while (m < 0 || m > 100);
+			nhap(B, m);
+			break;
+		case 3:
+			cout << "Xuat 2 danh sach:\n";
+			cout << "Danh sach A:\n";
+			xuat(A, n);
+			cout << "\nDanh sach B:\n";
+			xuat(B, m);
+			break;
+		case 4:
+			cout << "Tong 2 da thuc la\n";
+			sum = add(A, B, n, m); 
+			size = max(n, m); 
+			xuat(sum, size); 
+			break;
+		case 5:
+			cout << "Hieu 2 da thuc la\n";
+			hieu = sub(A, B, n, m); 
+			size = max(n, m); 
+			xuat(hieu, size); 
+			break;
+		case 6:
+			cout << "Tich 2 da thuc la\n";
+			tich = mul(A, B, n, m);  
+			xuat(tich, n + m - 1); 
+			break;
+		case 7:
+			cout << "Thuong 2 da thuc la\n";
+			chiadathuc(A, B, n, m);
+			break;
+		default:
+			cout << "Gia tri chon khong hop le\n";
+			break;
+		}
+	}
+	return 0;
+}
